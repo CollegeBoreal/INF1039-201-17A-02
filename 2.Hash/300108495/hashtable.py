db75c0643bf1df2e6d245d2a71f6ed9777d5467d
safaa <safaazaoui@gmail.com>
2017-10-05 15:50:01 -0400

    safaa

---
+++ 2.Hash/300108495/hashtable.py
@@ -0,0 +1,10 @@
+Python 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:19:30) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> book = dict()
+
+>>> book[" Apple "] = 0.67
+>>> book[" milk "] = 1.49
+>>> book[" avocado "] =1,49
+>>> print book
+{' avocado ': (1, 49), ' milk ': 1.49, ' Apple ': 0.67}
+>>> 
