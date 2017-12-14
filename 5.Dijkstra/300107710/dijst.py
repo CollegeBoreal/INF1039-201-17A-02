765027caec00d82759563a3662323bfc82ac1ac7
Bouchichi <31969059+Bouchichi@users.noreply.github.com>
2017-12-14 15:32:47 -0500

    Add files via upload

--- 5.Dijkstra/300107710/dijst.py
+++ 5.Dijkstra/300107710/dijst.py
@@ -5,13 +5,14 @@ graph["start"]["b"] = 2
 
 graph["a"] = {}
 graph["a"]["fin"] = 1
+
 graph["b"] = {}
 graph["b"]["a"] = 3
 graph["b"]["fin"] = 5
+
 graph["fin"] = {}
 
-infinity = float("infinity")
-
+infinity = float("inf")
 costs = {}
 costs["a"] = 6
 costs["b"] = 2
@@ -20,35 +21,34 @@ costs["fin"] = infinity
 parents = {}
 parents["a"] = "start"
 parents["b"] = "start"
-parents["fin"] = "none"
+parents["fin"] = None
 
 processed = []
 
-def node(costs):
-	node = find_lowest_cost_node(costs)
-	while node is not None:
-		cost = costs[node]
-		neighbors = graph[node]
-		for n in neighbors.key():
-			new_cost = cost + neighbors[n]
-			if costs[n] > new_cost:
-				costs[n] = node
-		processed.append(node)
-		node = find_lowest_cost_node(costs)
-
-def find_lowest_cost_node():
+def find_lowest_cost_node(costs):
 	lowest_cost = float("inf")
-	lowest_cost_node = 0
+	lowest_cost_node = None
 	for node in costs:
-		cost = cost[node]
-
-	if cost < lowest_cost and node not in processed:
-		lowest_cost = cost
-		lowest_cost_node = node
+		cost = costs[node]
+		if cost < lowest_cost and node not in processed:
+			lowest_cost = cost
+			lowest_cost_node = node
 	return lowest_cost_node
 
+node = find_lowest_cost_node(costs)
+while node is not None:
+		cost = costs[node]
+		neighbors = graph[node]
+		for n in neighbors.keys():
+			new_cost = cost + neighbors[n]
+			if costs[n] > new_cost:
+				costs[n] = new_cost
+				parents[n] = node
+		processed.append(node)
+		node = find_lowest_cost_node(costs)
 
 
 print("Costs from the start to each node:")
 print(costs)
+
 
